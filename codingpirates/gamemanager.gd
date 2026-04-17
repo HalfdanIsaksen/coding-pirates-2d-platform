795a3a6fa659d26dd3ec1427a07571a9ffa2032c
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2026-04-17 03:32:58 +0200

    Health and global assets

--- codingpirates/gamemanager.gd
+++ codingpirates/gamemanager.gd
@@ -1,9 +1,19 @@
 extends Node
 
+signal health_changed(current_health)
+
 var max_health := 3
 var health := 3
 
 func player_died(player: Node2D):
 	health -= 1
+	emit_signal("health_changed", health)
 	
+	if health <= 0:
+		health = max_health
+		emit_signal("health_changed", health)
+		print("Game Over")
+		get_tree().reload_current_scene()
+		return
 	
+	get_tree().reload_current_scene()

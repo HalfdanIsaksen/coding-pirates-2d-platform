31b7a97204980e89f5e613cf394382fa26f973bc
Halfdan Albrecht Isaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2026-04-23 18:28:50 +0200

    23 april

--- codingpirates/health.gd
+++ codingpirates/health.gd
@@ -7,7 +7,8 @@ extends CanvasLayer
 ]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Gamemanager.health_changed.connect(update_hearts)
+	update_hearts(Gamemanager.health)
 
 func update_hearts(current_health: int) -> void:
 	for i in range(hearts.size()):
